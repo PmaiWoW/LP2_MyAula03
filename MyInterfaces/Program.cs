7efbb2ab4030d2101d05d3fec75f4912f27537c1
PmaiWoW <PmaiWoW@Gmail.com>
2019-10-14 08:11:05 +0100

    commit mostly empty class rep

---
+++ MyInterfaces/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyInterfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola".CompareTo("ola"));
+        }
+    }
+}
